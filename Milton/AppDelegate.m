97ea35df06b3f3f542f30709b33ef69f4fe61b7b
Chris Schroder <chris_schroeder@mac.com>
2016-05-19 08:48:15 -0400

    [#119768173] Sync point.

--- Milton/AppDelegate.m
+++ Milton/AppDelegate.m
@@ -24,6 +24,7 @@ void handleUncaughtException(NSException *exception) {
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   [EmbeddedAgent configureAgent];
+  EALogError(@"appcliation did finish launching");
   
   self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
   self.tabBarController = [[MiltonTabBarController alloc] init];
@@ -34,6 +35,7 @@ void handleUncaughtException(NSException *exception) {
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {
+  EALogError(@"applicationWillResignActive");
   /*
    Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
    Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
@@ -42,6 +44,7 @@ void handleUncaughtException(NSException *exception) {
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
+  EALogError(@"applicationDidEnterBackground");
   /*
    Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
    If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
@@ -50,12 +53,14 @@ void handleUncaughtException(NSException *exception) {
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
+  EALogError(@"applicationWillEnterForeground");
   /*
    Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
    */
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
+  EALogError(@"applicationDidBecomeActive");
 
 }
 
